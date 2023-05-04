81aacae828acceadf36c4d21b045033cabdca250
Daniel Fields <dgfields4193@gmail.com>
2023-05-04 12:23:56 -0400

    wip: edit styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        peach: {
+          50: '#FCF5EC'
+        },
+        blueish: {
+          50: '#21243D'
+        },
+      },
+      dropShadow: {
+        '4xl': [
+          '-1rem -1rem rgba(0, 0, 0, 0.55)',
+      ]
+      },
+      fontSize: {
+        xsm: '0.60rem',
+      },
     },
   },
   plugins: [],
