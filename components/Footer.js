18c9da847846d5cf5ff4a618c21b74589ef2503e
Daniel Fields <dgfields4193@gmail.com>
2023-05-06 18:53:46 -0400

    wip: add mobile design

--- components/Footer.js
+++ components/Footer.js
@@ -4,12 +4,12 @@ import Link from 'next/link';
 const Footer = (props) =>{
     const year = new Date().getFullYear()
     return (
-        <footer id='footer' className='flex justify-between w-screen items-center bg-blueish-50'>
+        <footer id='footer' className='max-w-mobile md:max-w-none flex justify-between items-center bg-blueish-50'>
         <div className='flex pl-5'>
-            <Link href='https://www.linkedin.com/in/daniel-fields-se/' target='_blank'><AiFillLinkedin className='text-peach-50 h-12 w-12 transition ease-in-out delay-50 hover:translate-y-1 hover:scale-110 duration-300'/></Link>
-            <Link href='https://github.com/danf8' target='_blank'><AiFillGithub className='text-peach-50 h-12 w-12 transition ease-in-out delay-50 hover:translate-y-1 hover:scale-110 duration-300'/></Link>
+            <Link href='https://www.linkedin.com/in/daniel-fields-se/' target='_blank'><AiFillLinkedin className='text-peach-50 h-12 w-12 md:transition md:ease-in-out md:delay-50 md:hover:translate-y-1 md:hover:scale-110 md:duration-300'/></Link>
+            <Link href='https://github.com/danf8' target='_blank'><AiFillGithub className='text-peach-50 h-12 w-12 md:transition md:ease-in-out md:delay-50 md:hover:translate-y-1 md:hover:scale-110 md:duration-300'/></Link>
         </div>
-        <p className= 'text-peach-50 text-sm pr-5'>Copyright &copy; All Rights Reserved Daniel Fields Portfolio {year}</p>
+        <p className= 'text-peach-50 text-center text-xs md:text-sm pr-5'>Copyright &copy; All Rights Reserved Daniel Fields Portfolio {year}</p>
         <style jsx>{`
            footer {
             height: 97px;
--- components/Main.js
+++ components/Main.js
@@ -9,27 +9,17 @@ const Main = () => {
 
     const showProject = projectData.map((p, i) => {
        return(
-             <section key={i} id='p1' className='py-12 grid grid-row-3 grid-cols-3 grid-flow-col '>
-                <h2 className='font-bold col-start-2 col-span-2 text-3xl text-bg-blueish-50 justify-self-start self-end my-5 w-49'>{p.name}</h2>
-                <p className=' not-italic col-start-2 col-span-2 row-start-2 text-bg-blueish-50 justify-self-start self-start '>{p.description}</p>
-                <Link className='col-start-2 col-end-2 row-start-2 self-end' href={p['github-frontend']} target='_blank'><AiFillGithub className='transition ease-in-out delay-50 hover:translate-y-1 hover:scale-110 duration-300 h-10 w-10'/></Link>
+             <section key={i} id='p1' className='border-t border-blueish-50 md:m-zero-auto md:mb-23 h-117 md:h-61 md:w-224 py-6 md:py-12 grid grid-cols-2  md:grid-cols-3 md:grid-flow-col max-w-mobile md:max-w-none '>
+                <Image id='project-pic' height={242} width={331} className='w-80 drop-shadow-4xl col-span-2 md:row-span-2 md:col-start-1 md:col-end-2 md:col-span-1 justify-self-center self-center rounded-md' src={p.pic} alt=""  />
+                <h2 className='font-bold md:pl-16 md:col-start-2 md:col-span-2 text-3xl text-bg-blueish-50 md:justify-self-start self-center md:my-5 md:w-49'>{p.name}</h2>
+                <Link className='col-start-2 col-end-2 row-start-2 md:pl-16 self-center md:self-end' href={p['github-frontend']} target='_blank'><AiFillGithub className='transition ease-in-out delay-50 hover:translate-y-1 hover:scale-110 duration-300 w-9 h-9 md:h-10 md:w-10'/></Link>
                 {p['github-backend'] !== null ? (
-                   <Link className='text-xsm col-start-2 col-end-2 row-start-2 justify-self-end self-end ' href={p['github-backend']} target='_blank'><AiFillGithub className='transition ease-in-out delay-50 hover:translate-y-1 hover:scale-110 duration-300 h-6 w-6'/>Backend</Link>
-                   ) : ''}
-                   <Link className='col-start-2 col-end-2 row-start-2 justify-self-center self-end' href={p.deployed} target='_blank'><BiLink className='transition ease-in-out delay-50 hover:translate-y-1 hover:scale-110 duration-300 h-10 w-10' /></Link>
-                    <Image id='project-pic' height={256} width={240} className='drop-shadow-4xl row-span-2 col-start-1 col-end-2 col-span-1 justify-self-center self-center rounded-md' src={p.pic} alt=""  />
+                    <Link className='md:pl-16 text-xsm col-start-2 col-end-2 row-start-2 justify-self-end self-center md:self-end ' href={p['github-backend']} target='_blank'><AiFillGithub className='transition ease-in-out delay-50 hover:translate-y-1 hover:scale-110 duration-300 w-9 h-9 md:h-6 md:w-6'/>Backend</Link>
+                    ) : ''}
+                   <Link className='md:pl-16 col-start-2 col-end-2 row-start-2 justify-self-center self-center md:self-end' href={p.deployed} target='_blank'><BiLink className='transition ease-in-out delay-50 hover:translate-y-1 hover:scale-110 duration-300 w-9 h-9 md:h-10 md:w-10' /></Link>
+                    <p className='md:pl-16 not-italic  md:col-start-2 col-span-2 md:row-start-2 text-bg-blueish-50 md:justify-self-start self-end md:self-start '>{p.description}</p>
                 <style jsx>{`
-                   #p1{
-                      width: 898px;
-                      height: 242px;
-                      margin: 0 auto;
-                      border-top: 1px solid #21243D;
-                      margin-bottom: 48px;
-                   }
-                   #project-pic{
-                      width: auto;
-                      height: auto;
-                   }
+
                 `}</style>
           </section>
        );
@@ -38,7 +28,7 @@ const Main = () => {
      return (
         <>
             <Header/>
-            <main id='projects' name='projects' className='my-info bg-peach-50 pb-10'>
+            <main id='projects' name='projects' className='my-info bg-peach-50 pb-10 max-w-mobile md:max-w-none'>
                 <h1 id='featured' className='leading-10 text-center text-4xl'>Featured Works</h1>
                 {showProject}
             <style jsx>{`
